785d777f97fc0675393fe5e3a482f2ef5530a875
xuxu <xu829475@qq.com>
2020-03-25 13:06:40 +0800

    springcloud

---
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author xu.Sir
+ * @date 2020-02-18 17:27
+ **/
+@Configuration
+public class ApplicationContextConfig {
+    /**
+     * applicationContext.xml <bean id="" class=""> </bean>
+     * LoadBalanced 负载均衡
+     * @return
+     */
+    @Bean
+    /// 使用自定义负载均衡规则 @LoadBalanced
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+}
